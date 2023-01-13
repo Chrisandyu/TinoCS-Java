ff9a3cde87e404838334820ae9f4330436f10ef8
Vismay Bhargav <bhaavis1@gmail.com>
2023-01-13 12:39:22 -0800

    just worked on the class lab for a bit feeling cool might delete later uwu

--- Homework/InClassLabIRT/IsoRightTri.java
+++ Homework/InClassLabIRT/IsoRightTri.java
@@ -1,4 +1,29 @@
+import java.lang.Math;
+
 public class IsoRightTri{
+    public int legLength;
     
+    public IsoRightTri(){
+        legLength = 4;
+    }
     
+    public IsoRightTri(int legLength){
+        legLength = legLength;
+    }
+    
+    public int getLegLength(){
+        return legLength;
+    }
+    
+    public void setLegLength(int length){
+        legLength = length;
+    }
+    
+    public double calculateArea(){
+        return (legLength * legLength) / 2.0;
+    }
+    
+    public double calcHyp(){
+        return Math.sqrt(Math.pow(legLength, 2.0) + Math.pow(legLength, 2.0));
+    }
 }
