4871d57ee862d64c3bdd6d6fb4155018acbe075e
Vismay Achar Bhargav <vismay.bhargav@gmail.com>
2023-01-14 00:27:43 -0800

    made another method

--- Homework/InClassLabIRT/IsoRightTri.java
+++ Homework/InClassLabIRT/IsoRightTri.java
@@ -26,4 +26,17 @@ public class IsoRightTri{
     public double calcHyp(){
         return Math.sqrt(Math.pow(legLength, 2.0) + Math.pow(legLength, 2.0));
     }
+    
+    public double calcPerimeter(){
+        return (legLength * 2) + calcHyp();
+    }
+    
+    public void printTriangle(){
+        int currCount = 0;
+        for(int i = 0; i < legLength; i++){
+            for(int j = 0; j < legLength; i++){
+                System.out.println();
+            }
+        }
+    }
 }
